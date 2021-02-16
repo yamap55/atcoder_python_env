f5f50ce1f6417eba7b4b1605f743e66f880da37a
yamap55 <tetuya120@hotmail.com>
2021-02-16 23:02:51 +0900

    [update] template code

--- templates/py/main.py
+++ templates/py/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+import sys
+from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insort_right  # type: ignore
+from collections import Counter, defaultdict, deque  # type: ignore
+from fractions import gcd  # type: ignore
+from heapq import heapify, heappop, heappush, heappushpop, heapreplace, merge  # type: ignore
+from itertools import accumulate, combinations, permutations, product  # type: ignore
+
+
+def LI(): return list(map(int, sys.stdin.buffer.readline().split()))
+def I(): return int(sys.stdin.buffer.readline())
+def LS(): return sys.stdin.buffer.readline().rstrip().decode("utf-8").split()
+def S(): return sys.stdin.buffer.readline().rstrip().decode("utf-8")
+def IR(n): return [I() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def SR(n): return [S() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
+def SRL(n): return [list(S()) for _ in range(n)]
+def MSRL(n): return [[int(i) for i in list(S())] for _ in range(n)]
